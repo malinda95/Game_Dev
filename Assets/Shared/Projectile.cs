1900987d6717aca4d1a97078edd665e6652f0ad0
Unknown <amal95perera@gmail.com>
2019-01-13 23:03:51 +0530

    completed shooting target

--- Assets/Shared/Projectile.cs
+++ Assets/Shared/Projectile.cs
@@ -17,11 +17,17 @@ public class Projectile : MonoBehaviour {
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit, 5f))
+        {
+            CheckDestructable(hit.transform);
+        }
     }
 
-    void OnTriggerEnter(Collider other)
+    void CheckDestructable(Transform other)
     {
-        var destructable = other.transform.GetComponent<Destructable>();
+        var destructable = other.GetComponent<Destructable>();
         if (destructable == null)
             return;
         destructable.TakeDamage(damage);
