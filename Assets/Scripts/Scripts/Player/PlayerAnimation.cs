7a43dc07561138356a5bb6202e1590d489ab0a45
Kalana <edata.incore@gmail.com>
2019-01-21 15:44:57 +0530

    Player Gun Holder Fix and Camara boungry modified

--- Assets/Scripts/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Scripts/Player/PlayerAnimation.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour {
     Animator animator;
     InputController PlayerInputController;
+
+    [SerializeField]
+    private Camera Player_Camera;
     // Use this for initialization
    
 	void Awake () {
@@ -24,11 +27,21 @@ public class PlayerAnimation : MonoBehaviour {
         animator.SetBool("IsCrouched", PlayerInputController.IsCrouched);
 
         // up down animation 
-        //animator.SetFloat("AimAngle", PlayerAim.GetAngle());
+        animator.SetFloat("AimAngle", CheckAngle(Player_Camera.transform.eulerAngles.x));
         //// aiming
         //animator.SetBool("IsAiming",
-                         // GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMING || 
-                         //GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMEDFIRING);
+        // GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMING || 
+        //GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMEDFIRING);
 
+        Debug.Log(CheckAngle(Player_Camera.transform.eulerAngles.x));
+    }
+    public float CheckAngle(float value)
+    {
+        float angle = value - 180;
+
+        if (angle > 0)
+            return angle - 180;
+
+        return angle + 180;
     }
 }
