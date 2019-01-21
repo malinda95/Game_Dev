2a305f505a7fce4b560eb2307874c3188c16e5f4
Unknown <amal95perera@gmail.com>
2019-01-21 08:33:16 +0530

    time out for join game and cursor lock in pause menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,12 +50,27 @@ public class PlayerController : MonoBehaviour {
 	void Update ()
 	{
 		if (PauseMenu.IsOn)
-			return;
+        {
+            if (Cursor.lockState != CursorLockMode.None)
+                Cursor.lockState = CursorLockMode.None;
 
-		//Setting target position for spring
-		//This makes the physics act right when it comes to
-		//applying gravity when flying over objects
-		RaycastHit _hit;
+            motor.Move(Vector3.zero);
+            motor.Rotate(Vector3.zero);
+            motor.RotateCamera(0f);
+            
+            return;
+
+        }
+
+        if(Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        //Setting target position for spring
+        //This makes the physics act right when it comes to
+        //applying gravity when flying over objects
+        RaycastHit _hit;
 		if (Physics.Raycast (transform.position, Vector3.down, out _hit, 100f, environmentMask))
 		{
 			joint.targetPosition = new Vector3(0f, -_hit.point.y, 0f);
