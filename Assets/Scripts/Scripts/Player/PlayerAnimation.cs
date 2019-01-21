1f4e06d2caf99e5681e134b76d34baabc26b6a13
Kalana <edata.incore@gmail.com>
2019-01-21 08:51:52 +0530

    Add Player Animation

---
+++ Assets/Scripts/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    Animator animator;
+    // Use this for initialization
+   
+	void Awake () {
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        //animator.SetFloat("Horizontal", GameManager.Instance.InputController.Horizontal);
+
+        //// Use for transition
+        //animator.SetBool("IsWalking", GameManager.Instance.InputController.IsWalking);
+        //animator.SetBool("IsSprinting", GameManager.Instance.InputController.IsSprinting);
+        //animator.SetBool("IsCrouched", GameManager.Instance.InputController.IsCrouched);
+
+        //// up down animation 
+        //animator.SetFloat("AimAngle", PlayerAim.GetAngle());
+        //// aiming
+        //animator.SetBool("IsAiming",
+                         // GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMING || 
+                         //GameManager.Instance.LocalPlayer.PlayerState.WeaponState == PlayerState.EWeaponState.AIMEDFIRING);
+
+    }
+}
